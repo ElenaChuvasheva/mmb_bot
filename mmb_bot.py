54acc469cec07446f8f7dfc2be39b5127df188c7
Елена Чувашева <chuvashevalena@yandex.ru>
2022-08-31 14:38:51 +0300

    Real url

--- mmb_bot.py
+++ mmb_bot.py
@@ -12,8 +12,7 @@ from dotenv import load_dotenv
 from exceptions import (GetException, ParseException, UnavailableException,
                         err_msg)
 
-# url = 'https://mmb.progressor.ru/'
-url = 'http://127.0.0.1:8000/'
+url = 'https://mmb.progressor.ru/'
 
 load_dotenv()
 telegram_token = os.getenv('telegram_token')
